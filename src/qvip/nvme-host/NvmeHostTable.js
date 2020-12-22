770a8fe35fa9bae8762f11f4a8bc37fa59cb955e
Duc Tran <duc1810@gmail.com>
2020-12-22 09:04:36 +0700

    add resizable column

--- src/qvip/nvme-host/NvmeHostTable.js
+++ src/qvip/nvme-host/NvmeHostTable.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components';
-import { useTable, useFilters, useColumnOrder } from 'react-table';
+import { useTable, useFilters, useColumnOrder,
+         useBlockLayout, useResizeColumns } from 'react-table';
 import { motion, AnimatePresence } from 'framer-motion';
 
 const InitHiddenColumns = [
@@ -18,29 +19,56 @@ const InitHiddenColumns = [
 ];
 
 const Styles = styled.div`
-padding: 1rem;
-table {
-  border-spacing: 0;
-  border: 1px solid black;
-  tr {
-    :last-child {
-      td {
-        border-bottom: 0;
-      }
+    padding: 1rem;
+
+    .table {
+        display: inline-block;
+        border-spacing: 0;
+        border: 1px solid black;
+
+        .tr {
+            :last-child {
+                .td {
+                    border-bottom: 0;
+                }
+            }
+        }
+
+        .th {
+            word-break: break-word;
+        }
+
+        .th,
+        .td {
+            margin: 0;
+            padding: 0.5rem;
+            border-bottom: 1px solid black;
+            border-right: 1px solid black;
+            
+            ${'' /* In this example we use an absolutely position resizer, so this is required. */}
+            position: relative;
+            :last-child {
+                border-right: 0;
+            }
+            .resizer {
+                display: inline-block;
+                background: blue;
+                width: 10px;
+                height: 100%;
+                position: absolute;
+                right: 0;
+                top: 0;
+                transform: translateX(50%);
+                z-index: 1;
+                ${'' /* prevents from scrolling while dragging on touch devices */}
+                touch-action:none;
+
+                &.isResizing {
+                    background: red;
+                }
+            }
+        }
     }
-  }
-  th,
-  td {
-    margin: 0;
-    padding: 0.5rem;
-    border-bottom: 1px solid black;
-    border-right: 1px solid black;
-    background: white;
-    :last-child {
-      border-right: 0;
-    }
-  }
-}
 `
 
 // Define a default UI for filtering
@@ -104,7 +132,7 @@ function SelectColumnFilter({
                             return arr;
                         }, []);
                         console.log("FF", options, filterValues);
-                        setFilter(filterValues.length==0 ? undefined : filterValues);
+                        setFilter(filterValues.length===0 ? undefined : filterValues);
                     }}
                 ></input>{option}
             </label>)}
@@ -122,7 +150,7 @@ function shuffle(arr) {
     return shuffled
 }
 
-function Table({ columns, data }) {
+function Table1({ columns, data }) {
     const defaultColumn = React.useMemo(
         () => ({
             // Let's set up our default Filter UI
@@ -239,7 +267,109 @@ function Table({ columns, data }) {
     )
 }
 
-const initFilterableHeader = ["BDF", "SRC", "RW", "TYPE"]
+function Table({ columns, data }) {
+    const defaultColumn = React.useMemo(
+        () => ({
+            minWidth: 30,
+            width: 150,
+            maxWidth: 400,
+            Filter: DefaultColumnFilter, // Default Filter
+        }),
+        []
+    )
+  
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        visibleColumns,
+        prepareRow,
+        allColumns,
+        setColumnOrder,
+        state,
+        resetResizing,
+    } = useTable(
+        {
+            columns,
+            data,
+            defaultColumn,
+            initialState: {hiddenColumns: InitHiddenColumns},
+        },
+        useColumnOrder,
+        useFilters,
+        useBlockLayout,
+        useResizeColumns
+    )
+  
+    return (
+        <>
+            <div className="globalOptions">
+                {allColumns.map(column => <div key={column.id}>
+                    <label><input type="checkbox" {...column.getToggleHiddenProps()}/>{column.id}</label>
+                </div>)}
+            </div>
+
+            <button onClick={resetResizing}>Reset Resizing</button>
+            <div>
+            <div {...getTableProps()} className="table">
+                <div>
+                {headerGroups.map(headerGroup => (
+                    <div {...headerGroup.getHeaderGroupProps()} className="tr">
+                    {headerGroup.headers.map(column => (
+                        <div {...column.getHeaderProps()} className="th">
+                            {column.render('Header')}
+                            <div>{column.canFilter ? column.render('Filter') : null}</div>
+                            {/* Use column.getResizerProps to hook up the events correctly */}
+                            <div
+                                {...column.getResizerProps()}
+                                className={`resizer ${
+                                column.isResizing ? 'isResizing' : ''
+                                }`}
+                            />
+                        </div>
+                    ))}
+                    </div>
+                ))}
+                </div>
+    
+                <div {...getTableBodyProps()}>
+                {rows.map((row, i) => {
+                    prepareRow(row)
+                    return (
+                    <div {...row.getRowProps()} className="tr">
+                        {row.cells.map(cell => {
+                        return (
+                            <div {...cell.getCellProps()} className="td">
+                            {cell.render('Cell')}
+                            </div>
+                        )
+                        })}
+                    </div>
+                    )
+                })}
+                </div>
+            </div>
+            </div>
+            <pre>
+            <code>{JSON.stringify(state, null, 2)}</code>
+            </pre>
+        </>
+    )
+}
+
+const initFilterableHeader = ["BDF", "SRC", "RW", "TYPE"];
+const initColumnWidth = {
+    "BDF": 80,
+    "SRC": 60,
+    "RW": 60,
+    "TYPE": 85,
+    "REG_NAME/QENTRY": 100,
+    "ADDR": 180,
+    "NSID": 95,
+    "DATA": 140,
+    "__default__": 150,
+}
 
 const NvmeHostTable = ({desc, headers, items}) => {
     const [filterableHeader, setFilterHeader] = React.useState(headers.reduce((obj, val) => {
@@ -254,8 +384,9 @@ const NvmeHostTable = ({desc, headers, items}) => {
             Filter: SelectColumnFilter,
             filter: 'includesSome',
             disableFilters: !filterableHeader[val],
+            width: (val in initColumnWidth) ? initColumnWidth[val] : initColumnWidth["__default__"],
         }
-    }), []);
+    }), [filterableHeader]);
 
     const data = React.useMemo(() => items, []);
 
